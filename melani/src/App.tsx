2451ffb061e15f67fd57a0cefba9d24ddef6b013
kaliktg <86680782+kaliktg@users.noreply.github.com>
2025-08-19 13:39:23 +0300

    dashbaord comppnent added

--- melani/src/App.tsx
+++ melani/src/App.tsx
@@ -2,14 +2,15 @@
 import { Routes, Route } from 'react-router-dom';
 import Melani from './restaurants/Melani';
 import Home from './Home';
-import Dashboard from './admin/Dashboard';
+import Page from './app/dashboard/Page'
+// import Dashboard from './admin/Dashboard';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/melani" element={<Melani />} />
-      <Route path="/admin" element={<Dashboard />} />
+      <Route path="/admin" element={<Page />} />
       {/* Add more restaurants here, e.g. <Route path="/cocoa" element={<Cocoa />} /> */}
       {/* Optionally, add a NotFound page: <Route path="*" element={<NotFound />} /> */}
     </Routes>
